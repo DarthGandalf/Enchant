DarthGandalf/Enchant

commit 765dba4f51eb6d4785e6f15cc0e077a340b1561c

Unicode normalization for comparing suggestion lists
git-svn-id: http://svn.abisource.com/enchant/trunk@22318 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/enchant.c
@@ -580,9 +580,16 @@ enchant_dict_merge_suggestions(EnchantDict * dict,
 				copy = 0;
 			else
 				{
+					char * normalized_new_sugg;
+
+					normalized_new_sugg = g_utf8_normalize (new_suggs[i], sugg_len, G_NORMALIZE_NFD);
+
 					for(j = 0; j < n_suggs; j++) 
 						{
-							if(strcmp(suggs[j],new_suggs[i])==0) 
+							char* normalized_sugg;
+							normalized_sugg = g_utf8_normalize (suggs[j], -1, G_NORMALIZE_NFD);
+
+							if(strcmp(normalized_sugg,normalized_new_sugg)==0) 
 								{
 									copy = 0; /*duplicate*/
 									break;

unittests/dictionary/enchant_dict_suggest_tests.cpp
@@ -444,6 +444,8 @@ TEST_FIXTURE(EnchantDictionarySuggest_TestFixture,
 
     ExternalAddWordToDictionary(Convert(L"fiance\x301")); // NFD u0301 = Combining acute accent
 
+    ReloadTestDictionary();
+
     size_t cSuggestions;
     _suggestions = enchant_dict_suggest(_dict, "fiance", -1, &cSuggestions);
     CHECK(_suggestions);

commit 7f7192d223337ea3e47fc2b5b35a931dada2170d

fix memory leak introduced in r22318
git-svn-id: http://svn.abisource.com/enchant/trunk@22319 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/enchant.c
@@ -594,7 +594,9 @@ enchant_dict_merge_suggestions(EnchantDict * dict,
 									copy = 0; /*duplicate*/
 									break;
 								}
+							g_free(normalized_sugg);
 						}
+					g_free(normalized_new_sugg);
 				}
 
 			if(copy)